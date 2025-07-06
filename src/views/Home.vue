5430cd5a8ede5cd7827fd9926b65e82e34955183
Francisco Lizcano <flizcanor@icloud.com>
2025-07-05 22:00:50 -0500

    Config inicial

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -22,11 +22,9 @@
             .content-wrapper.px-lg-4.pt-lg-4.px-xxl-5.py-xxl-5.text-start
               p.home-main-title UNIDAD {{ `${globalData.numeroUnidad}` }}
               h1.home-title.mb-4 {{ globalData.tituloUnidad }}
-              p.descripcion.mb-4 Desde los comienzos de la humanidad, la relación entre los principios éticos, el desarrollo económico y la sostenibilidad ha sido fundamental para garantizar un progreso equilibrado y equitativo. En un contexto globalizado, donde los desafíos éticos y ambientales son cada vez más evidentes, la bioética emerge como una herramienta clave para orientar decisiones económicas responsables que respeten tanto el medio ambiente como los derechos humanos.
-
-              p.descripcion.mb-4 En esta unidad, profundizaremos en el rol de la bioética como pilar de la sostenibilidad en el desarrollo económico, explorando temas esenciales como la aplicación de principios éticos al uso de recursos naturales, la influencia de las políticas globales en la protección de los derechos humanos y la ética en la innovación tecnológica. 
+              p.descripcion.mb-4 Desde hace décadas, la neurociencia ha ofrecido descubrimientos esenciales sobre el funcionamiento del cerebro humano y su impacto en los procesos educativos. En esta unidad, se exploran modelos fundamentales que explican cómo el cerebro procesa la información, aprende, siente y actúa, con el fin de optimizar la enseñanza y personalizar los aprendizajes.
               
-              p #[b Prepárate para explorar los retos y oportunidades que la sostenibilidad y la bioética presentan en un mundo en constante evolución.]
+              p #[b ¡Prepárese para transformar su práctica educativa desde la ciencia del cerebro y la comprensión de los estilos de aprendizaje!]
 
               .button-container.mt-auto
                 router-link.btn-iniciar.w-100.w-md-auto(:to="{ name: 'introduccion' }")
